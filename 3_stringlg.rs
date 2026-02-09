67d29ab9721db58dbd9bfaedaebdc903d3a8dc50
Vasim Sheikh <vasimsheikh@Vasims-MacBook-Air.local>
2026-02-10 02:30:45 +0530

    string ways

--- 3_stringlg.rs
+++ 3_stringlg.rs
@@ -7,3 +7,15 @@ fn main (){
     let my_str_length= get_string_length(&v);
     println!("The no of characters in my string is :{}",my_str_length);
 }
+
+//////////////
+//Another way
+// fn main (){
+//     let v= String::from("hello world");
+//     let my_str_length= get_string_length(v);
+//     println!("The no of characters in my string is :{}",my_str_length);
+// }
+// fn get_string_length(s :String)-> usize {
+//     s.chars().count()
+//     // no. return no semicolon then implicit return 
+// }
